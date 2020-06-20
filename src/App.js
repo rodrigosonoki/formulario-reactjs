ac4e7eae626ba057aafc28bfb47a3a85185f1cb6
Rodrigo Sonoki <rodrigosonoki@iMac-de-Rodrigo.local>
2020-06-19 21:18:47 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+const credentials = require("./credentials.json");
+const { GoogleSpreadsheet } = require("google-spreadsheet");
+const idSheet = "1JGkfTQR_T5xAzM-XkDPUUuOq4_b9UMWFkoQ3NHpiM7s";
 
 function App() {
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+
+  var data = {
+    name,
+    phone,
+  };
+
+  async function connectToApi(e) {
+    //CONNECT
+    e.preventDefault();
+    const file = new GoogleSpreadsheet(idSheet);
+    await file.useServiceAccountAuth({
+      client_email: credentials.client_email,
+      private_key: credentials.private_key,
+    });
+    await file.loadInfo();
+    const sheet = file.sheetsByIndex[0];
+    await sheet.addRow({
+      nome: `${data.name}`,
+      telefone: `${data.phone}`,
+      data: new Date(),
+    });
+
+    //
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form>
+        <h1>Digite seus dados abaixo</h1>
+        <label>Nome</label>
+        <input type="text" onChange={(event) => setName(event.target.value)} />
+        <label>Telefone</label>
+        <input type="tel" onChange={(event) => setPhone(event.target.value)} />
+        <button type="submit" onClick={connectToApi}>
+          ENVIAR
+        </button>
+      </form>
     </div>
   );
 }
